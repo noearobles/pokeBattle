ff2a7b8d2d731f396eb0952ab83fe148dbbcab0a
noaarobles <roblesnoe1030@gmail.com>
2023-02-14 05:19:59 -0600

    modified js

--- server/index.js
+++ server/index.js
@@ -1,11 +1,11 @@
 // const api_key = process.env.PASS_WORD;
-let input = document.querySelector("#pokemonName");
-input.addEventListener("keyup", (e) => {
-  if (e.keyCode === 13) {
-    document.querySelector("#search").click();
-  }
-});
-document.querySelector("#search").addEventListener("click", getPokemon);
+let pokemons = [];
+const poke_card = document.getElementById("pokemonBox");
+const url = "https://pokeapi.co/api/v2/pokemon";
+const pMon_num = 151;
+const search = document.querySelector("#pokemonName");
+const input = document.getElementById("pokemonName");
+
 function capitalizeFirstLetter(string) {
   return string.charAt(0).toUpperCase() + string.slice(1);
 }
@@ -16,55 +16,117 @@ function convertHeightAndWeight(num) {
   return num / 10;
 }
 
-function getPokemon(e) {
-  const name = document.querySelector("#pokemonName").value;
-  const pokemonName = lowerCaseName(name);
-
-  fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`)
-    .then((response) => response.json())
-    .then((data) => {
-      document.querySelector(
-        ".pokemonBox"
-      ).innerHTML = `<div><img class="img-container"
-         src="${data.sprites.other["official-artwork"].front_default}" alt="${
-        data.name
-      }">
-        </div><div class="pokemonInfo"><h1 id="name">${capitalizeFirstLetter(
-          data.name
-        )}</h1>
-        <p id="weight">Weight: ${convertHeightAndWeight(data.weight)}kg</p>
-        <p id="height">Height: ${convertHeightAndWeight(
-          data.height
-        )}m</p><p id="type">Type: ${capitalizeFirstLetter(
-        data.types.map((type) => type.type.name).join(", ")
-      )}</p>
-        </div>`;
-    })
-    .catch((err) => {
-      console.log("Pokemon not found", err);
-    });
-  e.preventDefault();
-}
-const pokedex = () => {
-  const promises = [];
-  for (i = 1; i < 155; i++) {
-    const url = `https://pokeapi.co/api/v2/pokemon/${i}`;
-    promises.push(fetch(url).then((res) => res.json()));
+const pokedex = async () => {
+  for (let i = 1; i <= pMon_num; i++) {
+    await getAllPMon(i);
   }
-  Promise.all(promises).then((results) => {
-    const pokemon = results.map((data) => ({
-      name: data.name,
-      id: data.id,
-      image: data.sprites.other["official-artwork"].front_default,
-      height: data.height,
-      weight: data.weight,
-    }));
-    displayPokemon(pokemon);
-  });
+  pokemons.forEach((pokemon) => createPMonCard(pokemon));
 };
 
-const displayPokemon = (pokemon) => {
-  console.log(pokemon);
+const extractPmon = () => {
+  const pMonEls = document.getElementsByClassName("pokemon");
+  let possiblePMon = [];
+  for (let i = 0; i < pMonEls.length; i++) {
+    const pMonEl = pMonEls[i];
+    possiblePMon = [...possiblePMon, pMonEl];
+  }
+  possiblePMon.forEach((possPMon) => possPMon.remove());
+};
+
+const getPMon = async (id) => {
+  const searchPMons = pokemons.filter((touch) => touch.name === id);
+  extractPmon();
+  searchPMons.forEach((pmon) => createPMonCard(pmon));
+};
+
+const getAllPMon = async (id) => {
+  const res = await fetch(`${url}/${id}`);
+  const pMon = await res.json();
+  pokemons = [...pokemons, pMon];
 };
 
 pokedex();
+
+function createPMonCard(pokemon) {
+  const pMonEl = document.createElement("div");
+  pMonEl.classList.add("pokemon");
+  const pMonType = pokemon.types.map((el) => el.type.name).slice(0, 1);
+  const name = pokemon.name[0].toUpperCase() + pokemon.name.slice(1);
+  const pMonStat = pokemon.stats.map((el) => el.stat.name);
+  const stats = pMonStat.slice(0, 3);
+  const baseValue = pokemon.stats.map((el) => el.base_stat);
+  const baseStat = baseValue.slice(0, 3);
+  const stat = stats
+    .map((stat) => {
+      return `<li class="names">${stat}<li>`;
+    })
+    .join("");
+  const base = baseStat
+    .map((base) => {
+      return `<li class="base">${base}</li>`;
+    })
+    .join("");
+  const pokeInnerHTML = `<div class="img-container">
+  <img src="${pokemon.sprites.other["official-artwork"].front_default}" 
+  alt = "${name}"/>
+  </div>
+  <div class="info">
+  <span class="number">#${pokemon.id.toString().padStart(3, "0")}</span>
+  <h3 class="name">${name}</h3>
+  <small class="type"><span>${pMonType}</span></small>
+  </div>
+  <div class ="stats">
+  <h2>Stats</h2>
+  <div class="flex">
+  <ul>${stat}</ul>
+  <ul>${base}</ul>
+  </div>
+  </div>`;
+  pMonEl.innerHTML = pokeInnerHTML;
+  poke_card.appendChild(pMonEl);
+}
+
+input.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const searchTerm = search.value;
+  if (searchTerm) {
+    getPMon(searchTerm);
+    search.value = "";
+  } else if (searchTerm === "") {
+    pokemons = [];
+    extractPmon();
+    pokedex();
+  }
+});
+
+// document.querySelector("#search").addEventListener("click", getPokemon);
+
+// function getPokemon(e) {
+//   const name = document.querySelector("#pokemonName").value;
+//   const pokemonName = lowerCaseName(name);
+
+//   fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`)
+//     .then((response) => response.json())
+//     .then((data) => {
+//       document.querySelector(
+//         ".pokemonBox"
+//       ).innerHTML = `<div><img class="img-container"
+//          src="${data.sprites.other["official-artwork"].front_default}" alt="${
+//         data.name
+//       }">
+//         </div><div class="pokemonInfo"><h1 id="name">${capitalizeFirstLetter(
+//           data.name
+//         )}</h1>
+//         <p id="weight">Weight: ${convertHeightAndWeight(data.weight)}kg</p>
+//         <p id="height">Height: ${convertHeightAndWeight(
+//           data.height
+//         )}m</p><p id="type">Type: ${capitalizeFirstLetter(
+//         data.types.map((type) => type.type.name).join(", ")
+//       )}</p>
+//         </div>`;
+//     })
+//     .catch((err) => {
+//       console.log("Pokemon not found", err);
+//     });
+//   e.preventDefault();
+// }
