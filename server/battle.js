45abbdf249a1207a34771fc38adacc12f71a39eb
noaarobles <roblesnoe1030@gmail.com>
2023-02-20 03:03:56 -0600

    modified js

--- server/battle.js
+++ server/battle.js
@@ -4,7 +4,11 @@ images = [
   "https://preview.redd.it/d9spuwer2c491.png?width=1050&format=png&auto=webp&s=9ca8c75c63da9f8bb134e955d73e2770d073375e",
   "https://i.pinimg.com/736x/89/04/3f/89043fb2d56b3583cce79efe1c3fb53d.jpg",
   "https://fiverr-res.cloudinary.com/images/q_auto,f_auto/gigs/204364595/original/86db6005cd51b4f60e71cca277f603a82cf5646a/draw-a-pixel-pokemon-battle-background.png",
-  "https://cutewallpaper.org/cdn-cgi/mirage/91b98c5373d6a01796e55b9f978389531ff4e5ebae85abd935c54ab8d42fdd46/1280/21/pokemon-battle-backgrounds/Pokemon-X-and-Y-battle-background-10-by-PhoenixOfLight92-on-.png"
+  "https://cutewallpaper.org/cdn-cgi/mirage/91b98c5373d6a01796e55b9f978389531ff4e5ebae85abd935c54ab8d42fdd46/1280/21/pokemon-battle-backgrounds/Pokemon-X-and-Y-battle-background-10-by-PhoenixOfLight92-on-.png",
+  "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/2fb2821a-1406-4a1d-9b04-6668f278e944/d843okx-eb13e8e4-0fa4-4fa9-968a-e0f36ff168de.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcLzJmYjI4MjFhLTE0MDYtNGExZC05YjA0LTY2NjhmMjc4ZTk0NFwvZDg0M29reC1lYjEzZThlNC0wZmE0LTRmYTktOTY4YS1lMGYzNmZmMTY4ZGUucG5nIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.TIK_E5L8dTyBUk_dADA5WkLP8jSJMR7YGJG54KNAido",
+  "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/2fb2821a-1406-4a1d-9b04-6668f278e944/d83og9h-f3e2558c-6085-478f-899b-882bf6cb62d1.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcLzJmYjI4MjFhLTE0MDYtNGExZC05YjA0LTY2NjhmMjc4ZTk0NFwvZDgzb2c5aC1mM2UyNTU4Yy02MDg1LTQ3OGYtODk5Yi04ODJiZjZjYjYyZDEuanBnIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.UJwQi7Yfd001rT6JFVfbFN4DmL6ZmE2iBPBqpv6f-DA",
+  "https://fiverr-res.cloudinary.com/images/t_main1,q_auto,f_auto,q_auto,f_auto/attachments/delivery/asset/412696943ecf62e9ee477e085adbea07-1648378716/RockForest/draw-a-pixel-pokemon-battle-background.gif",
+  "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/2fb2821a-1406-4a1d-9b04-6668f278e944/d83m36e-3b58ca48-fe8a-456e-9ffc-a5a84eca6613.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcLzJmYjI4MjFhLTE0MDYtNGExZC05YjA0LTY2NjhmMjc4ZTk0NFwvZDgzbTM2ZS0zYjU4Y2E0OC1mZThhLTQ1NmUtOWZmYy1hNWE4NGVjYTY2MTMucG5nIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.jOpoPaOypPatcb4k7flznTP9YiwUEX2q2BKoeWUPU74",
 ];
 let imgCount = images.length;
 let number = Math.floor(Math.random() * imgCount);
@@ -115,6 +119,30 @@ let pkmArr = [
       ["Low Sweep", 65, 1],
     ],
   ],
+  [
+    "Arbok",
+    "https://projectpokemon.org/images/sprites-models/normal-back/arbok.gif",
+    "https://projectpokemon.org/images/normal-sprite/arbok.gif",
+    324,
+    [
+      ["Acid", "poison", 40, 1],
+      ["Sludge Bomb", "Poison", 90, 1],
+      ["Fire Fang", "fire", 65, 0.95],
+      ["Gunk Shot", 120, 0.8],
+    ],
+  ],
+  [
+    "Aerodactyl",
+    "https://projectpokemon.org/images/sprites-models/normal-back/aerodactyl.gif",
+    "https://projectpokemon.org/images/normal-sprite/aerodactyl.gif",
+    364,
+    [
+      ["Stone Edge", "rock", 80, 1],
+      ["Wing Attack", "fly", 60, 1],
+      ["Hyper Beam", "normal", 150, 0.9],
+      ["Rock Slide", 75, 0.9],
+    ],
+  ],
 ];
 //pk immunities [0], weaknesses [1], resistances[2]//
 let typeMatch = {
@@ -129,14 +157,19 @@ let typeMatch = {
     ["fire", "fly", "ice", "steel"],
     ["grass", "water", "electric", "fighting"],
   ],
-  Pikachu: [["electric"], ["ground"], ["flying", "steel"]],
+  Pikachu: [["electric"], ["ground"], ["fly", "steel"]],
   Gengar: [
     ["normal", "fighting"],
     ["ground", "psychic", "ghost", "dark"],
     ["grass", "posion", "bug", "fairy"],
   ],
   Mewtwo: [[""], ["bug", "ghost", "dark"], ["psychic", "fighting"]],
-  Machamp: [[""], ["flying", "psychic", "fairy"], ["dark", "rock", "bug"]],
+  Arbok: [[""], ["ground", "psychic"], ["fighting", "poison", "bug", "fairy"]],
+  Aerodactyl: [
+    ["ground"],
+    ["water", "rock", "steel", "electric", "ice"],
+    ["fire", "poison", "bug", "normal", "fly"],
+  ],
 };
 //function to spawn pk, true for player1, false for foe//
 function spawn(bool) {
